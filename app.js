b3dc5f865ef9c52a3f822cc16383983d1a092e08
Daniela Santos Barrios <isc18350501@gmail.com>
2021-12-01 00:15:03 -0600

    Se agrego express y dotenv

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hola Dai");
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res){res.send ('Hola Dai');});
+
+app.listen(3000);
+//localhost:3000
